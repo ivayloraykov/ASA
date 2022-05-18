c091eb835a3bf22ad8802e38f4e11b3faf826931
ivayloraykov <ivailo_raikov@hotmail.com>
2022-05-18 08:29:34 +0300

    Add comments

--- wifi_button_buzzer_oled.ino.ino
+++ wifi_button_buzzer_oled.ino.ino
@@ -4,21 +4,22 @@
 #include <Adafruit_AM2320.h>
 #include <SoftwareSerial.h>
 #include "dht.h"
-#define dht_apin A0
-#define OLED_RESET 4
+#define dht_apin A0 //A0 pin DHT11 sensor
+#define OLED_RESET 4 // 4 pin refresh oled display
 
 //User defined variables
-const String roomName = "Server room 1";
-int smsTimeOut = 0;
-#define numberCount 1
-String recievers[numberCount] = { "+359883286555" };
-int rainDropSensorPin = A1;
+const String roomName = "Server room 1"; // the name of the room
+int messageDelay = 10000; // time out delay in seconds befor next SMS 
+#define numberCount 2 // numbers of phone
+String recievers[numberCount] = { "+359883286555","+359884994335","+359884994333"}; // declarate phones which will receive sms
+int rainDropSensorPin = A1; // Rain drop sensor pin
 
 dht DHT;
 SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
 
 const int BUTTON = 7; // Button sensor Pin
 const int buzzer = 5; // Buzzer Pin
+int smsTimeOut = 0;
 Adafruit_SSD1306 display(OLED_RESET);
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
 
@@ -32,38 +33,38 @@ void setup()
 
   //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   mySerial.begin(9600);
-
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
-  updateSerial();
-  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
-  updateSerial();
-  mySerial.println("AT+CMGS=\"+359883286555\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
-  updateSerial();
-  mySerial.print("Arduino works !!! "); //text content
-  updateSerial();
-  mySerial.write(26);
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
+  for (int i = 0; i < numberCount; i++)
+  {
+    mySerial.println("AT"); updateSerial(); //Once the handshake test is successful, it will back to OK
+    mySerial.println("AT+CMGF=1");
+    mySerial.println("AT+CMGS=\"" + recievers[i] + "\""); updateSerial();
+    mySerial.print("Arduino works"); updateSerial(); //text content
+    mySerial.write(26);
+    Serial.println("AT+CMGS=\"" + recievers[i] + "\"");
+    delay(3000);
+  }
+  
   pinMode(buzzer, OUTPUT);
 
 }
 
 void displayTempHumid()
 {
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0, 0);
+  display.clearDisplay(); // clear display
+  display.setTextColor(WHITE); // display color
+  display.setTextSize(1); // display text size
+  display.setCursor(0, 0); //cursor position
 
-  display.print(roomName);
-  display.setCursor(0, 10);
+  display.print(roomName); // Show room name on the display
+  display.setCursor(0, 10); // Position on the display
 
-  display.print("Humidity: " + String(DHT.humidity, 2) + " %");
+  display.print("Humidity: " + String(DHT.humidity, 2) + " %"); // Show humidity on the display
   display.setCursor(0, 20);
 
-  display.print("Temperature: " + String(DHT.temperature, 2) + " C");
+  display.print("Temperature: " + String(DHT.temperature, 2) + " C"); // Show temperature on the display
 }
 
-void sendSMStoNumbers(String msg)
+void sendSMStoNumbers(String msg) // function for sending SMS
 {
   for (int i = 0; i < numberCount; i++)
   {
@@ -78,31 +79,28 @@ void sendSMStoNumbers(String msg)
 
 void loop()
 {
-  displayTempHumid();
+  displayTempHumid(); // Show humidity and temperature on the display
   display.display();  
-  DHT.read11(dht_apin);
+  DHT.read11(dht_apin); // read information from DHT11 sensor
 
-  Serial.print("Water sensor value: ");
-  Serial.println(analogRead(rainDropSensorPin));
-
-  if (DHT.temperature > 28 && smsTimeOut <= 0)
+  if (DHT.temperature > 28 && smsTimeOut <= 0) // If Temperature is over 28C you will receive sms
   {
     sendSMStoNumbers("Temperature is over 28C !!!");
-    smsTimeOut = 10000;
+    smsTimeOut = messageDelay;
   }
-  else if (digitalRead(BUTTON) == HIGH) 
+  else if (digitalRead(BUTTON) == HIGH) // If you push the button you will receive sms with Temperature and Humidity right now 
   {
     tone(buzzer, 1000); // Send 1KHz sound signal...
     delay(1000);
     noTone(buzzer);     // Stop sound...
     sendSMStoNumbers("Current humidity = " + String(DHT.humidity, 2) + "%" + "Current temperature = " + String(DHT.temperature, 2) + "C");
   }
-  else if (analogRead(rainDropSensorPin) < 700 && smsTimeOut <= 0)
+  else if (analogRead(rainDropSensorPin) < 700 && smsTimeOut <= 0)// If Rain drop value (1024) is less then 700 you will receive sms
   {
     sendSMStoNumbers("Water sensor activated !!!");
-    smsTimeOut = 10000;
+    smsTimeOut = messageDelay;
   }
-  else if (smsTimeOut > 0)
+  else if (smsTimeOut > 0) // smsTimeOut will start
   {smsTimeOut --;}
 }
 
